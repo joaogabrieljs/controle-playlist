fdf20ee7c811a2f8531c8a806821c6cb78f94ab5
Joao Gabriel Junqueira <j.g.junqueira@hotmail.com>
2017-12-31 02:03:53 -0200

    Added routes to list and add songs

--- routes/musicas.js
+++ routes/musicas.js
@@ -4,9 +4,15 @@ var router = express.Router();
 var db = require('../db');
 
 router.get('/', (req, res, next) => {
+    db("musicas").then((musicas)=>{
+        res.render('index', {
+            musicas: musicas
+        });
+    },next);
 });
 
 router.get('/add', (req, res, next) => {
+    res.render('add');
 });
 
 router.post('/', (req, res, next) => {
